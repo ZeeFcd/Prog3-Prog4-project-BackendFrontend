047f2c0834bea9e6c8bee15badd8577ae068df63
Zoli <Zoli@DESKTOP-T7QO7U7>
2021-10-10 16:15:12 +0200

    Add project files.

---
+++ IJA9WQ_HFT_2021221/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IJA9WQ_HFT_2021221
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
