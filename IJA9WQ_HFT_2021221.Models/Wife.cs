54da24c03b7f8ee7c5f169078fa48fc2265ddca4
ZeeF <ZeeF@stud.uni-obuda.hu>
2021-10-16 17:52:18 +0200

    3. class Wedding létrehozása

---
+++ IJA9WQ_HFT_2021221.Models/Wife.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IJA9WQ_HFT_2021221.Models
+{
+    public class Wife
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [NotMapped]
+        public virtual Husband Husband { get; set; }
+
+        [NotMapped]
+        public virtual Wedding Wedding { get; set; }
+
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
