ab7b84c77d27f219abd0f431fa299b918106dd33
ZeeF <ZeeF@stud.uni-obuda.hu>
2021-10-16 17:43:07 +0200

    Husban class létrehozáésa

---
+++ IJA9WQ_HFT_2021221.Models/Husband.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IJA9WQ_HFT_2021221.Models
+{
+    public class Husband
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [NotMapped]
+        public virtual Wife Wife { get; set; }
+
+        [ForeignKey(nameof(Wife))]
+        public int WifeID { get; set; }
+
+        [NotMapped]
+        public virtual Wedding Wedding { get; set; }
+
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+
+
+    }
+}
