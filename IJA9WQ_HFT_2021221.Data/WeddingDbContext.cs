83060a11a3d7eadd639debe631b880eec2a13a7b
ZeeF <ZeeF@stud.uni-obuda.hu>
2021-10-16 18:19:09 +0200

    Dbcontext fálj létrehozása nudget packek meghícása data projban

---
+++ IJA9WQ_HFT_2021221.Data/WeddingDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IJA9WQ_HFT_2021221.Data
+{
+    class WeddingDbContext : DbContext
+    {
+
+
+    }
+}
