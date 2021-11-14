a93792de38b4bbb2d5b460d3e3c8da28a0a56ac1
ZeeF <ZeeF@stud.uni-obuda.hu>
2021-11-14 14:39:24 +0100

    wife repo és az iterface elkészítése

---
+++ IJA9WQ_HFT_2021221.Repository/IWifeRepository.cs
@@ -0,0 +1,14 @@
+﻿using IJA9WQ_HFT_2021221.Models;
+using System.Linq;
+
+namespace IJA9WQ_HFT_2021221.Repository
+{
+    public interface IWifeRepository
+    {
+        void Create(Wife Wife);
+        void Delete(int id);
+        Wife Read(int id);
+        IQueryable<Wife> ReadAll();
+        void Update(Wife Wife);
+    }
+}
--- IJA9WQ_HFT_2021221.Repository/WifeRepository.cs
+++ IJA9WQ_HFT_2021221.Repository/WifeRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using IJA9WQ_HFT_2021221.Data;
+using IJA9WQ_HFT_2021221.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,46 @@ using System.Threading.Tasks;
 
 namespace IJA9WQ_HFT_2021221.Repository
 {
-    public class  WifeRepository
+    public class WifeRepository : IWifeRepository
     {
+        WeddingDbContext db;
+        public WifeRepository(WeddingDbContext db)
+        {
+            this.db = db;
+        }
+
+        public void Create(Wife Wife)
+        {
+            db.Wives.Add(Wife);
+            db.SaveChanges();
+        }
+
+        public Wife Read(int id)
+        {
+            return
+                db.Wives.FirstOrDefault(t => t.Id == id);
+        }
+
+        public IQueryable<Wife> ReadAll()
+        {
+            return db.Wives;
+        }
+
+        public void Delete(int id)
+        {
+            var WifeToDelete = Read(id);
+            db.Wives.Remove(WifeToDelete);
+            db.SaveChanges();
+        }
+
+        public void Update(Wife Wife)
+        {
+            var WifeToUpdate = Read(Wife.Id);
+            WifeToUpdate.Name = Wife.Name;
+            WifeToUpdate.Age = Wife.Age;
+
+
+            db.SaveChanges();
+        }
     }
 }
