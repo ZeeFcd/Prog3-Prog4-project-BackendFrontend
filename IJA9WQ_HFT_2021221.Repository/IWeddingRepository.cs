7e223e38c6c7e099f786509981ba0f94b6c7da5a
ZeeF <ZeeF@stud.uni-obuda.hu>
2021-11-14 14:33:45 +0100

    Wedding repo és a hozzá tartozó interface elkészítése

---
+++ IJA9WQ_HFT_2021221.Repository/IWeddingRepository.cs
@@ -0,0 +1,14 @@
+﻿using IJA9WQ_HFT_2021221.Models;
+using System.Linq;
+
+namespace IJA9WQ_HFT_2021221.Repository
+{
+    public interface IWeddingRepository
+    {
+        void Create(Wedding wedding);
+        void Delete(int id);
+        Wedding Read(int id);
+        IQueryable<Wedding> ReadAll();
+        void Update(Wedding wedding);
+    }
+}
--- IJA9WQ_HFT_2021221.Repository/WeddingRepository.cs
+++ IJA9WQ_HFT_2021221.Repository/WeddingRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using IJA9WQ_HFT_2021221.Data;
+using IJA9WQ_HFT_2021221.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,47 @@ using System.Threading.Tasks;
 
 namespace IJA9WQ_HFT_2021221.Repository
 {
-    public class WeddingRepository
+    public class WeddingRepository : IWeddingRepository
     {
+        WeddingDbContext db;
+        public WeddingRepository(WeddingDbContext db)
+        {
+            this.db = db;
+        }
+
+        public void Create(Wedding wedding)
+        {
+            db.Weddings.Add(wedding);
+            db.SaveChanges();
+        }
+
+        public Wedding Read(int id)
+        {
+            return
+                db.Weddings.FirstOrDefault(t => t.Id == id);
+        }
+
+        public IQueryable<Wedding> ReadAll()
+        {
+            return db.Weddings;
+        }
+
+        public void Delete(int id)
+        {
+            var WeddingToDelete = Read(id);
+            db.Weddings.Remove(WeddingToDelete);
+            db.SaveChanges();
+        }
+
+        public void Update(Wedding wedding)
+        {
+            var WeddingToUpdate = Read(wedding.Id);
+            WeddingToUpdate.Place = wedding.Place;
+            WeddingToUpdate.Price = wedding.Price;
+            WeddingToUpdate.HusbandID = wedding.HusbandID;
+            WeddingToUpdate.WifeID = wedding.WifeID;
+
+            db.SaveChanges();
+        }
     }
 }
