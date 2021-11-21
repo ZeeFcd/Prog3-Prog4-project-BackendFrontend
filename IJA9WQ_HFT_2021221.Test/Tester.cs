ff669b9095ccb39f1e09494dd788a1e01cdc855f
ZeeF <ZeeF@stud.uni-obuda.hu>
2021-11-21 19:32:32 +0100

    Logic create methódusokra unit test

--- IJA9WQ_HFT_2021221.Test/Tester.cs
+++ IJA9WQ_HFT_2021221.Test/Tester.cs
@@ -1,4 +1,7 @@
 ﻿using IJA9WQ_HFT_2021221.Logic;
+using IJA9WQ_HFT_2021221.Models;
+using IJA9WQ_HFT_2021221.Repository;
+using Moq;
 using NUnit.Framework;
 using System;
 
@@ -14,22 +17,61 @@ namespace IJA9WQ_HFT_2021221.Test
         [SetUp]
         public void Init()
         {
+            var mockHusbandRepository = new Mock<IHusbandRepository>();
+            var mockWifeRepository = new Mock<IWifeRepository>();
+            var mockWeddingRepository = new Mock<IWeddingRepository>();
+
+
+            h = new HusbandLogic(mockHusbandRepository.Object);
+            wi = new WifeLogic(mockWifeRepository.Object);
+            w = new WeddingLogic(mockWeddingRepository.Object);
 
         }
 
-        [Test]
-        public void HusbandLogicCreateTest()
+        [TestCase(27,true)]
+        [TestCase(15, false)]
+        public void HusbandLogicCreateTest(int age,bool result)
         {
+            if (result)
+            {
+                Assert.That(() => h.Create(new Husband() { Age = age }), Throws.Nothing);
+            }
+            else
+            {
+                Assert.That(() => h.Create(new Husband() { Age = age }), Throws.Exception);
+            }
+            
         }
 
-        [Test]
-        public void WifeLogicCreateTest()
+        [TestCase(16, true)]
+        [TestCase(15, false)]
+        public void WifeLogicCreateTest(int age, bool result)
         {
+            if (result)
+            {
+                Assert.That(() => wi.Create(new Wife() { Age = age }), Throws.Nothing);
+            }
+            else
+            {
+                Assert.That(() => wi.Create(new Wife() { Age = age }), Throws.Exception);
+            }
+            
         }
 
-        [Test]
-        public void WeddingLogicCreateTest()
+        [TestCase(5000, true)]
+        [TestCase(-5000, false)]
+        public void WeddingLogicCreateTest(int prize, bool result)
         {
+            if (result)
+            {
+                
+                Assert.That(() => w.Create(new Wedding() { Price = prize }), Throws.Nothing);
+            }
+            else
+            {
+                Assert.That(() => w.Create(new Wedding() { Price = prize }), Throws.Exception);
+            }
+           
         }
 
         [Test]
