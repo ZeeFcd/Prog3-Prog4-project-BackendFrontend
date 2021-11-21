e2093df70a4755e81e02fcdba0a05e6dae935425
ZeeF <ZeeF@stud.uni-obuda.hu>
2021-11-21 18:29:54 +0100

    Tester osztály és a hozzá tartozó metódusok létrehozása(üres metódusok egyelőre)

--- IJA9WQ_HFT_2021221.Test/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace IJA9WQ_HFT_2021221.Test
-{
-    public class Class1
-    {
-    }
-}
---
+++ IJA9WQ_HFT_2021221.Test/Tester.cs
@@ -0,0 +1,61 @@
+﻿using IJA9WQ_HFT_2021221.Logic;
+using NUnit.Framework;
+using System;
+
+namespace IJA9WQ_HFT_2021221.Test
+{
+    [TestFixture]
+    public class Tester
+    {
+        HusbandLogic h;
+        WifeLogic wi;
+        WeddingLogic w;
+
+        [SetUp]
+        public void Init()
+        {
+
+        }
+
+        [Test]
+        public void HusbandLogicCreateTest()
+        {
+        }
+
+        [Test]
+        public void WifeLogicCreateTest()
+        {
+        }
+
+        [Test]
+        public void WeddingLogicCreateTest()
+        {
+        }
+
+        [Test]
+        public void MarriedCouplesTest()
+        { 
+        }
+
+        [Test]
+        public void AverageAgeTest() 
+        { 
+        }
+
+        [Test]
+        public void WeddingPlacesByWifeTest()
+        { 
+        }
+
+        [Test]
+        public void WeddingPricesByHusbandTest()
+        { 
+        }
+
+        [Test]
+        public void WifeWhereHusbandIsOldestTest()
+        { 
+        }
+
+    }
+}
