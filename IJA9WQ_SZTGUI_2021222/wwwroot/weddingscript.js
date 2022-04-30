3619fda881b41eed9e1eebf360d7c998ef257aac
ZeeF <ZeeF@stud.uni-obuda.hu>
2022-04-30 20:44:27 +0200

    project DONE

--- IJA9WQ_SZTGUI_2021222/wwwroot/weddingscript.js
+++ IJA9WQ_SZTGUI_2021222/wwwroot/weddingscript.js
@@ -1,5 +1,6 @@
 ﻿let weddings = [];
 let connection = null;
+let weddingIDToUpdate = -1;
 getweddingdata();
 setupSignalRwedding();
 
@@ -45,8 +46,33 @@ async function getweddingdata() {
         .then(y => {
             weddings = y;
             console.log(weddings);
-            //display();
+            display();
         });
+}
+
+function display() {
+    document.getElementById('resultarea').innerHTML = "";
+    weddings.forEach(t => {
+        document.getElementById('resultarea').innerHTML +=
+            "<tr><td>" + t.id + "</td><td>"
+            + t.place + "</td><td>" +
+            + t.price + "</td><td>" +
+            + t.husbandID + "</td><td>" +
+            + t.wifeID + "</td><td>" +
+            `<button type="button" onclick="remove(${t.id})">Delete</button>` +
+            `<button type="button" onclick="showupdate(${t.id})">Update</button>`
+            + "</td></tr>";
+    });
+}
+
+function showupdate(id) {
+
+    document.getElementById('weddingplacetoupdate').value = weddings.find(t => t['id'] == id)['place'];
+    document.getElementById('pricetoupdate').value = weddings.find(t => t['id'] == id)['price'];
+    document.getElementById('husbandidtoupdate').value = weddings.find(t => t['id'] == id)['husbandID'];
+    document.getElementById('wifeidtoupdate').value = weddings.find(t => t['id'] == id)['wifeID'];
+    document.getElementById('updateformdiv').style.display = 'flex';
+    weddingIDToUpdate = id;
 }
 
 function remove(id) {
@@ -65,12 +91,35 @@ function remove(id) {
 }
 
 function create() {
-    let name = document.getElementById('actorname').value;
+    let place = document.getElementById('weddingplace').value;
+    let price = document.getElementById('price').value;
+    let husbandid = document.getElementById('husbandid').value;
+    let wifeid = document.getElementById('wifeid').value;
     fetch('http://localhost:18885/wedding', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json', },
         body: JSON.stringify(
-            { actorName: name })
+            { Place: place, Price: price, HusbandID: husbandid, WifeID: wifeid })
+    })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getweddingdata();
+        })
+        .catch((error) => { console.error('Error:', error); });
+
+}
+
+function update() {
+    let place = document.getElementById('weddingplacetoupdate').value;
+    let price = document.getElementById('pricetoupdate').value;
+    let husbandid = document.getElementById('husbandidtoupdate').value;
+    let wifeid = document.getElementById('wifeidtoupdate').value;
+    fetch('http://localhost:18885/wedding', {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json', },
+        body: JSON.stringify(
+            { Id: weddingIDToUpdate, HusbandID: husbandid, WifeID: wifeid, Place: place, Price: price })
     })
         .then(response => response)
         .then(data => {
